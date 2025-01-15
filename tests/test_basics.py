905d7fbdc9eaa4ac93d292ac28181215e5de5074
greyhoundforty <ryantiffany@fastmail.com>
2025-01-15 13:16:15 -0600

    Updating tests to pick up env

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from datetime import datetime
 
@@ -5,6 +6,10 @@ from datetime import datetime
 def test_script_execution():
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     custom_domain = f"test{timestamp}.gh40-dev.systems"
+    env = os.environ.copy()
+    assert "DO_TOKEN" in env, "DO_TOKEN environment variable is not set"
+    assert "IBMCLOUD_API_KEY" in env, "IBMCLOUD_API_KEY environment variable is not set"
+
     result = subprocess.run(
         [
             "python",
@@ -22,6 +27,7 @@ def test_script_execution():
         ],
         capture_output=True,
         text=True,
+        env=env,
     )
     assert result.returncode == 0
     # Example of using curl to check the resulting custom domain
